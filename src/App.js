878d88af13136cc2f5204b1b187267f3b583c997
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-09-26 17:52:47 +0500

    latest

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPage from './components/MainPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from "./components/MainPage";
 import RegisterUser from "./components/RegisterUser";
-import UpdateStudents from './components/UpdateStudents';
+import UpdateStudents from "./components/UpdateStudents";
+import DeleteUser from "./components/deleteUser";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/registeruser" element={<RegisterUser />} />
           <Route path="/updatestudents/:userId" element={<UpdateStudents />} />
+          <Route path="/userdelete/:id" element={<DeleteUser />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/deleteUser.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function DeleteUser() {
+  const { id } = useParams();
+
+  useEffect(() => {
+    const deleteUser = async () => {
+      try {
+        const response = await fetch(`http://localhost:4000/api/delete/${id}`, {
+          method: "DELETE",
+        });
+
+        if (response.ok) {
+          console.log("User Deleted Successfully");
+        } else {
+          console.log("User not Deleted Successfully");
+        }
+      } catch (error) {
+        console.error("Delete error:", error);
+      }
+    };
+    deleteUser();
+  }, [id]);
+
+  return (
+    <div>
+      <h1>User Deleted</h1>
+    </div>
+  );
+}
+
+export default DeleteUser;
